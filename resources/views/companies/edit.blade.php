9fe0b556e5967f3ea99c4711105a555227d16a3c
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2023-02-08 01:30:37 +0900

    wip

--- resources/views/companies/edit.blade.php
+++ resources/views/companies/edit.blade.php
@@ -32,7 +32,11 @@
                     <div class="col-xs-12 col-sm-12 col-md-12">
                         <div class="form-group">
                             <strong>Company Name:</strong>
-                            <input type="text" name="name" value="{{ $company->name }}" class="form-control"
+                            <input
+                                   type="text"
+                                   name="name"
+                                   value="{{ old('name', $company->name ) }}"
+                                   class="form-control"
                                    placeholder="Company name">
                             @error('name')
                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
@@ -42,8 +46,12 @@
                     <div class="col-xs-12 col-sm-12 col-md-12">
                         <div class="form-group">
                             <strong>Company Email:</strong>
-                            <input type="email" name="email" class="form-control" placeholder="Company Email"
-                                   value="{{ $company->email }}">
+                            <input
+                                   type="email"
+                                   name="email"
+                                   class="form-control"
+                                   placeholder="Company Email"
+                                   value="{{ old('email', $company->email ) }}">
                             @error('email')
                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                             @enderror
@@ -52,7 +60,11 @@
                     <div class="col-xs-12 col-sm-12 col-md-12">
                         <div class="form-group">
                             <strong>Company Address:</strong>
-                            <input type="text" name="address" value="{{ $company->address }}" class="form-control"
+                            <input
+                                   type="text"
+                                   name="address"
+                                   value="{{ old('address', $company->address ) }}"
+                                   class="form-control"
                                    placeholder="Company Address">
                             @error('address')
                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
